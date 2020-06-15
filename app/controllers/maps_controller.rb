22727de9726d11f7b4f822f83f04d22e9f77eaa3
Nazo777 <7nazo-san7@ezweb.ne.jp>
2020-06-15 19:37:59 +0900

    AddCreateActionController

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,21 @@
 class MapsController < ApplicationController
   def index
+    @map = List.new
+    @maps = List.all
+  end
+
+  def create
+    @map = List.new(list_params)
+    if @map.save
+      redirect_to maps_url
+    else
+      @maps = List.all
+      render 'maps/index'
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:address,:latitude, :longitude, :title, :message)
   end
 end
